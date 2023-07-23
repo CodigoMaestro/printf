b66d00772f7b352a64a55c70b4a1aaaa75c64239
codigomaestro <williamsroland147@outlook.com>
2023-07-23 23:22:31 +0100

    formated d and i

--- main.h
+++ main.h
@@ -9,5 +9,10 @@ int _printf(const char *format, ...);
 size_t _strlen(const char *str);
 int process_format_specifier(char format, va_list args, int count);
 int process_format(const char *format, va_list args);
+int print_char(char c);
+int print_string(const char *str);
+int print_integer(int num);
+
+int print_int(va_list args);
 
 #endif
