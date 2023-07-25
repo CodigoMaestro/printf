41ead5f266ba5a9c19b0e5f947603584460248d5
duncanram <94283243+duncanram@users.noreply.github.com>
2023-07-25 23:16:29 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -2,28 +2,27 @@
 #include <string.h>
 
 /**
- * _printf - Prints formatted output based on the format specifier.
- * @format: A character string containing format specifiers.
- *
- * Return: The number of characters printed (excluding the null byte).
+ * _printf - entry point
+ * @format: char
+ * Return: 0
  */
 
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count;
+        va_list args;
+        int count;
 
-	va_start(args, format);
+        va_start(args, format);
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
+        if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+                return (-1);
 
-	if (strcmp(format, "% ") == 0 || strcmp(format, "%") == 0)
-		return (-1);
+        if (strcmp(format, "% ") == 0 || strcmp(format, "%") == 0)
+                return (-1);
 
-	count = process_format(format, args);
+        count = process_format(format, args);
 
-	va_end(args);
-	return (count);
+        va_end(args);
+        return (count);
 }
 
